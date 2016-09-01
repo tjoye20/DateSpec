898fa635a96ab8f6e5376573cfd4e89df44ecd63
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-01 01:41:42 -0500

    Pass all current tests.Add tests for destroy action.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -54,11 +55,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.use_transactional_fixtures = false
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
 
-  config.before(:each) do
+  config.before(:each) do 
     DatabaseCleaner.clean
   end
 
