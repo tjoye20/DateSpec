ecd015f46e078cd4189d9a023d86bc44c894b345
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-04 18:26:54 -0500

    Start writing specs for admirer controller.

--- spec/controllers/admirers_controller_spec.rb
+++ spec/controllers/admirers_controller_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 
 describe AdmirersController do
-  let!(:user) {User.create(username: "tj", email: "test@test.com", password: "password",
+  let(:user) {User.create(username: "tj", email: "test@test.com", password: "password",
     gender: "male", gender_seeking: "female", bio: "Nigerian. Developer.", question_1: "Do you want kids?.",
     question_2: "What do you think of the #BlackLivesMatter movement?", question_3: "Are you open to adoption?")}
-  let!(:user2) {User.create(username: "Tasha", email: "t4tasha@test.com", password: "password",
+
+  let(:user2) {User.create(username: "Tasha", email: "t4tasha@test.com", password: "password",
     gender: "female", gender_seeking: "male", bio: "Jamaican. Engineer. I love cats!", question_1: "Do you like cats?",
     question_2: "Have you ever been married?", question_3: "Do you have any kids?")}
 
-  let!(admirer){Admirer.create(user_id: user.id, admirer_id: user2.id, q1_response: , q2_response: , q3_response:)}
+  let(:admirer) { Admirer.create(user_id: user.id, admirer_id: user2.id, q1_response: "Yes, I want kids.", q2_response: "Woo, that's a long convo I'd prefer to
+    have in person, so nothing gets miscrontrued.", q3_response: "No, I don't have any kids!")}
 
   before(:each) do
     session[:user_id] = user.id
   end
 
+  let(:admirers) { Admirer.all }
+  let(:new_admirer) { Admirer.new }
+
   describe "GET #index" do
     it "responds with status code 200" do
       get :index
       expect(response).to be_success
-      expect(assigns(:users)).to eq(user.id) #only 1 user created
+      expect(admirers).to eq [admirer] #only 1 user created
       expect(response).to have_http_status 200
       expect(response).to render_template(:index)
     end
@@ -28,7 +33,7 @@ describe AdmirersController do
     it "responds with status code 200" do
       get :new
       expect(response).to be_success
-      expect(assigns(:user)).to be_a_new User
+      expect(new_admirer).to be_a_new Admirer
       expect(response).to have_http_status 200
       expect(response).to render_template(:new)
     end
@@ -38,7 +43,7 @@ describe AdmirersController do
     it "responds with status code 200" do
       get :show, {id: user.id}
       expect(response).to be_success
-      expect(assigns(:user)).to eq(user)
+      expect(admirer.admirer_id).to eq user2.id
       expect(response).to have_http_status 200
       expect(response).to render_template(:show)
     end
@@ -52,7 +57,7 @@ describe AdmirersController do
         question_2: "Test question 2.", question_3: "Test question 3."}}
       end
 
-      it "responds with status code 302" do
+      xit "responds with status code 302" do
         expect{post(:create, params)}.to change(User, :count).by(1)
         expect(response).to have_http_status 302
         expect(session[:user_id]).to eq(User.last.id)
@@ -65,7 +70,7 @@ describe AdmirersController do
       let :params do
         {user: {username: "Tom", email: "t4tom@test.com"}}
       end
-      it "responds with status code 200" do
+      xit "responds with status code 200" do
         post :create, params
         expect(response).to have_http_status 200
         expect(response).to render_template(:new)
@@ -74,7 +79,7 @@ describe AdmirersController do
   end
 
   describe "DELETE #destroy" do
-    it "logs out user, clears the session, and sets active to false" do
+    xit "logs out user, clears the session, and sets active to false" do
       delete :destroy, {id: tasha.id}
       expect(session[:user_id]).to eq nil
       expect(assigns(:tasha)).to eq nil
